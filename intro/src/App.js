a41e097ce611ad09d61146e35804b2a1a3a35467
jmbeach <jaredbeachdesign@gmail.com>
2020-12-20 17:32:42 -0600

    video 1 done

--- intro/src/App.js
+++ intro/src/App.js
@@ -3,6 +3,9 @@ import './index.css';
 function App() {
   return (
     <div className="App">
+      <div className="bg-gray-200">Text</div>
+      <div className="bg-white">Body</div>
+      <div className="bg-blue-800">Footer</div>
     </div>
   );
 }
